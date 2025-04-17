b400f685705530c73fa8e6b44fd0cb88dbb48286
Saunder Stock <saunderstock2@gmail.com>
2025-04-17 08:57:30 -0400

    create FromStandardLibrary.kt (resolves #9)

---
+++ base-util/src/main/kotlin/FromStandardLibrary.kt
@@ -0,0 +1,20 @@
+package com.s12works.imageRandomizer
+
+/**
+ * indicates the disconnected nature of the specified feature
+ *
+ * This (and [StandardLibraryUtility]) allows for programatic/direct
+ * awareness of whether or not a certain feature is seperate from
+ * other potentially related API's; this awareness is intended to
+ * indicate the potential instability of said feature as a result.
+ *
+ * @see [StandardLibraryUtility] includes officially supported types
+ */
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS
+)
+public annotation class FromStandardLibrary(
+    public val utilityType: StandardLibraryUtility
+)

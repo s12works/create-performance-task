298f1ddaf6d87ae23462b71771c79979efbb9ad6
Saunder Stock <saunderstock2@gmail.com>
2025-04-17 08:57:55 -0400

    create StandardLibraryUtility.kt

---
+++ base-util/src/main/kotlin/StandardLibraryUtility.kt
@@ -0,0 +1,17 @@
+package com.s12works.imageRandomizer
+
+/**
+ * represents a supported type of standard-library utility
+ *
+ * This (and [FromStandardLibrary]) allows for programatic/direct
+ * awareness of whether or not a certain feature is seperate from
+ * other potentially related API's; this awareness is intended to
+ * indicate the potential instability of said feature as a result.
+ *
+ * @see [FromStandardLibrary] used for application
+ */
+public enum class StandardLibraryUtility {
+    ASSERTION_ENFORCER,
+    OBJECT_REPR_REPRESENTOR,
+    OBJECT_REPR_TRANSLATOR
+}

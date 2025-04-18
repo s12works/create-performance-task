03baa8e840c48c7b8efe89162b0ce3d109d98ed1
Saunder Stock <saunderstock2@gmail.com>
2025-04-17 20:28:08 -0400

    add object-repr subproject

---
+++ object-repr/build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.dokka.DokkaConfiguration.Visibility as DokkaVisibility
+
+dependencies {
+    implementation(project(":base-util"))
+    implementation(kotlin("reflect"))
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>().configureEach {
+    dokkaSourceSets.configureEach {
+        reportUndocumented.set(true)
+        sourceRoots.from(file("./src/main"))
+        documentedVisibilities.set(setOf<DokkaVisibility>(
+            DokkaVisibility.PUBLIC,
+            DokkaVisibility.PROTECTED,
+            DokkaVisibility.PRIVATE,
+            DokkaVisibility.INTERNAL
+        ))
+    }
+}

329be519b85c5d735c9d8adf62a6c0a70e49f595
Saunder Stock <saunderstock2@gmail.com>
2025-04-18 06:41:55 -0400

    fix typos

--- assertion/src/main/kotlin/assertion/Report.kt
+++ assertion/src/main/kotlin/assertion/Report.kt
@@ -3,12 +3,12 @@ package com.s12works.imageRandomizer.assertion
 /**
  * contains all that which is necessary for processing/representing errors
  * 
- * @property reciept represents the report's required [Reciept] (or representation of the error)
- * @property occurance represents the occurance of the error temporally
+ * @property receipt represents the report's required [Receipt] (or representation of the error)
+ * @property occurrence represents the occurance of the error temporally
  */
 public data class Report(
     public val reciept: Receipt,
-    public val occurance: java.time.LocalDateTime
+    public val occurrence: java.time.LocalDateTime
 ) {
     override fun toString(): com.s12works.imageRandomizer.objectRepr.Translation =
         com.s12works.imageRandomizer.objectRepr.getStandardRepr(this)

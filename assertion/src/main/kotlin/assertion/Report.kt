db2b7c6918fe3bc86d0c47c05c3d90622cbbb0af
Saunder Stock <saunderstock2@gmail.com>
2025-04-18 06:36:02 -0400

    create Report.kt

---
+++ assertion/src/main/kotlin/assertion/Report.kt
@@ -0,0 +1,15 @@
+package com.s12works.imageRandomizer.assertion
+
+/**
+ * contains all that which is necessary for processing/representing errors
+ * 
+ * @property receipt represents the report's required [Receipt] (or representation of the error)
+ * @property occurrence represents the occurance of the error temporally
+ */
+public data class Report(
+    public val receipt: Receipt,
+    public val occurrence: java.time.LocalDateTime
+) {
+    override fun toString(): com.s12works.imageRandomizer.objectRepr.Translation =
+        com.s12works.imageRandomizer.objectRepr.getStandardRepr(this)
+} 

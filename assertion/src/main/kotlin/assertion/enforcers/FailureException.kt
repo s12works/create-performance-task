f1b07ab4683b9723307faff4e7d593ca10e84395
Saunder Stock <saunderstock2@gmail.com>
2025-04-18 05:58:42 -0400

    create FailureException.kt

---
+++ assertion/src/main/kotlin/assertion/enforcers/FailureException.kt
@@ -0,0 +1,11 @@
+package com.s12works.imageRandomizer.assertion.enforcers
+
+public class FailureException internal constructor(
+    public val report: com.s12works.imageRandomizer.assertion.Report
+): Exception(report.receipt.message) {
+    override fun toString(): com.s12works.imageRandomizer.objectRepr.Translation =
+        com.s12works.imageRandomizer.objectRepr.getStandardRepr(this)
+ 
+    override fun equals(other: Any?): Boolean =
+        this com.s12works.imageRandomizer.standardObjectEquality.equals other
+}

6326f26d5a67522f495165ce7fcff6d896dc2c0a
Saunder Stock <saunderstock2@gmail.com>
2025-04-20 01:11:47 -0400

    create getImageFromComp.kt

---
+++ graphics/src/main/kotlin/graphics/getImageFromComp.kt
@@ -0,0 +1,30 @@
+package com.s12works.imageRandomizer.graphics
+
+import com.s12works.imageRandomizer.assertion.enforcers.fail
+
+public fun getImageFromComp(composition: SimplifiedImageComp, colorGenerator: (CategoryIDColorFormat) -> Color): Image {
+    val pixels = mutableListOf<Pixel<Color>>()
+    var resolution = Resolution(0, 0)
+
+    for (format in composition.keys) {
+        for (position in composition[format]!!) {
+            // update resolution to acquire maximum values
+            resolution = Resolution(
+                if (position.xpos > resolution.length) position.xpos else resolution.length,
+                if (position.ypos > resolution.width) position.ypos else resolution.width
+            )
+
+            pixels.add(
+                Pixel(
+                    position,
+                    colorGenerator(format)
+                )
+            )
+        }
+    }
+
+    return Image(
+        resolution,
+        pixels.toList()
+    )
+}

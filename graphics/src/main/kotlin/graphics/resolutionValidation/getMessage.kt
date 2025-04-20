05646243051c77480541436fb327ca7d4169fe17
Saunder Stock <saunderstock2@gmail.com>
2025-04-20 01:04:03 -0400

    update getMessage.kt

--- graphics/src/main/kotlin/graphics/resolutionValidation/getMessage.kt
+++ graphics/src/main/kotlin/graphics/resolutionValidation/getMessage.kt
@@ -1,4 +1,4 @@
 package com.s12works.imageRandomizer.graphics.resolutionValidation
 
 public fun getMessage(type: DimensionType, dimension: com.s12works.imageRandomizer.graphics.Dimension):
-    com.s12works.imageRandomizer.assertion.Message = "ImageRectangle object illegally instantiated; ${type.name.lowercase(java.util.Locale.US)} value is ${dimension}"
+    com.s12works.imageRandomizer.assertion.Message = "Resolution object illegally instantiated; ${type.name.lowercase(java.util.Locale.US)} value is $dimension"
